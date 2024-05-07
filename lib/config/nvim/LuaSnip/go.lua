9654532a3059a7924de1a054c3961a3eac494cbc
geemus <geemus@gmail.com>
2024-05-07 16:36:54 -0500

    reorganize and expand go snippets

--- lib/config/nvim/LuaSnip/go.lua
+++ lib/config/nvim/LuaSnip/go.lua
@@ -1,4 +1,166 @@
 return {
+  -- bubbletea
+  s({ trig = "tclient" },
+    fmta(
+      [[
+        var err error
+        clientCmd := &auth.Client{
+          Config: c.Config,
+          Anc:    c.anc,
+        }
+        c.anc, err = clientCmd.Perform(ctx, drv)
+        if err != nil {
+          return err
+        }
+      ]],
+      {}
+    )
+  ),
+  s({ trig = "tdone" },
+    fmta(
+      [[
+        fmt.Fprintln(&b, ui.StepDone("<>"))
+      ]],
+      {
+        i(1, "step")
+      }
+    )
+  ),
+  s({ trig = "theader" },
+    fmta(
+      [[
+        fmt.Fprintln(&b, ui.Header(fmt.Sprintf("<> %s", ui.Whisper("`<>`"))))
+      ]],
+      {
+        i(1, "header"),
+        i(2, "cmd")
+      }
+    )
+  ),
+  s({ trig = "thint" },
+    fmta(
+      [[
+        fmt.Fprintln(&b, ui.StepHint("<> %s"))
+      ]],
+      {
+        i(1, "hint")
+      }
+    )
+  ),
+  s({ trig = "tinprogress" },
+    fmta(
+      [[
+        fmt.Fprintln(&b, ui.StepInProgress(fmt.Sprintf("<>…%s", m.spinner.View())))
+      ]],
+      {
+        i(1, "step")
+      }
+    )
+  ),
+  s({ trig = "tm" },
+    fmta(
+      [[
+        type <> struct{}
+
+        func (m *<>) Init() tea.Cmd { return nil }
+
+        func (m *<>) Update(msg tea.Msg) (tea.Model, tea.Cmd) { return m, nil }
+
+        func (m *<>) View() string {
+          var b strings.Builder
+
+          return b.String()
+        }
+      ]],
+      {
+        i(1, "model"),
+        rep(1),
+        rep(1),
+        rep(1)
+      }
+    )
+  ),
+
+  -- fmt
+  s({ trig = "ff" },
+    fmta(
+      [[
+        fmt.Printf("%<>", <>)
+      ]],
+      {
+        i(1, "format"),
+        i(2, "var")
+      }
+    )
+  ),
+  s({ trig = "ffl" },
+    fmta(
+      [[
+        fmt.Fprintln(&<>, "<>")
+      ]],
+      {
+        i(1, "writer"),
+        i(2, "string")
+      }
+    )
+  ),
+  s({ trig = "fl" },
+    fmta(
+      [[
+        fmt.Println("<>")
+      ]],
+      {
+        i(1, "string"),
+      }
+    )
+  ),
+  s({ trig = "fs" },
+    fmta(
+      [[
+        fmt.Sprintf("%<>", <>)
+      ]],
+      {
+        i(1, "format"),
+        i(2, "var")
+      }
+    )
+  ),
+
+  -- logging
+  s({ trig = "lf" },
+    fmta(
+      [[
+        log.Printf("%<>", <>)
+      ]],
+      {
+        i(1, "s"),
+        i(2, "var")
+      }
+    )
+  ),
+  s({ trig = "ll" },
+    fmta(
+      [[
+        log.Println("<>")
+      ]],
+      {
+        i(1, "s"),
+      }
+    )
+  ),
+  s({ trig = "ls" },
+    fmta(
+      [[
+        log.Sprintf("%<>", <>)
+      ]],
+      {
+        i(1, "s"),
+        i(2, "var")
+      }
+    )
+  ),
+
+  -- misc
   s({ trig = "el" },
     fmta(
       [[
@@ -11,36 +173,14 @@ return {
       }
     )
   ),
-  s({ trig = "ff" },
+  s({ trig = "ie" },
     fmta(
       [[
-        log.Printf("%<>", <>)
+        if err != nil {
+          return err
+        }
       ]],
-      {
-        i(1, "s"),
-        i(2, "var")
-      }
-    )
-  ),
-  s({ trig = "fl" },
-    fmta(
-      [[
-        log.Println("<>")
-      ]],
-      {
-        i(1, "s"),
-      }
-    )
-  ),
-  s({ trig = "fs" },
-    fmta(
-      [[
-        log.Sprintf("%<>", <>)
-      ]],
-      {
-        i(1, "s"),
-        i(2, "var")
-      }
+      {}
     )
   ),
   s({ trig = "if" },
@@ -72,14 +212,30 @@ return {
       }
     )
   ),
-  s({ trig = "ir" },
+
+  -- testing
+  s({ trig = "tie" },
     fmta(
       [[
         if err != nil {
-          return err
+          t.Fatal(err)
         }
       ]],
       {}
     )
   ),
+  s({ trig = "tiwg" },
+    fmta(
+      [[
+        if want, got := <>, <>; want != got {
+          t.Errorf("want:\n\n%s\n\ngot:\n\n%s\n\n", want, got)
+        }
+      ]],
+      {
+        i(1, "want"),
+        i(2, "got")
+      }
+    )
+  ),
+
 }
