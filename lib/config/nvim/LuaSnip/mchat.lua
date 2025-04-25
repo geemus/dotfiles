1f428d57b3b8f0d2fc407eddebad73529e927f68
geemus <geemus@gmail.com>
2025-04-25 14:16:18 +0000

    [luasnip] expanding on mchat snippets

--- lib/config/nvim/LuaSnip/mchat.lua
+++ lib/config/nvim/LuaSnip/mchat.lua
@@ -1,11 +1,46 @@
 return {
+  s({ trig = "citations" },
+    fmt(
+      [[
+        - After answering, include a numbered list of citation urls in <citations>.
+
+      ]],
+      {}
+    )
+  ),
   s({ trig = "cot" },
     fmt(
       [[
-        - Do not preamble.
         - Before answering, think step-by-step in <cot>.
+
       ]],
       {}
+    )
+  ),
+  s({ trig = "dnp" },
+    fmt(
+      [[
+        - Do not preamble.
+
+      ]],
+      {}
+    )
+  ),
+  s({ trig = "system" },
+    fmt(
+      [=[
+        system = [[
+          - You are {}.
+          - Before answering, think step-by-step in <cot>.
+          - Do not preamble.
+          - Provide exact numbers and metrics where possible.
+          - Provide 1 to 100 score with feedback.
+          - Provide confidence percentages on claims.
+        ]]
+      ]=],
+      {
+        i(1, "role")
+      }
     )
   ),
 }
