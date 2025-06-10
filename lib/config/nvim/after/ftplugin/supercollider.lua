673b625572382eb5cbf1cb7903101fdf840b5840
geemus <geemus@gmail.com>
2025-06-10 15:57:17 +0000

    comment out autoload of sclang stuff, as it wasn't working well

--- lib/config/nvim/after/ftplugin/supercollider.lua
+++ lib/config/nvim/after/ftplugin/supercollider.lua
@@ -1,6 +1,4 @@
-local scnvim = require 'scnvim'
-local sclang = require 'scnvim.sclang'
+--local sclang = require 'scnvim.sclang'
 
-scnvim.start()
-
-scnvim.send("(s.boot;)")
+--sclang.start()
+--sclang.send("(s.boot;)", false)
