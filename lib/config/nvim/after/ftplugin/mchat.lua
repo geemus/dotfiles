849c9982b7814d339d0a89ce8f71ab0974618f23
geemus <geemus@gmail.com>
2025-07-09 14:19:15 +0000

    [nvim] default to wrapped lines for mchats

--- lib/config/nvim/after/ftplugin/mchat.lua
+++ lib/config/nvim/after/ftplugin/mchat.lua
@@ -1,8 +1,5 @@
---
--- FOLLOW MD LINKS
---
-
-local nvim_buf_set_keymap = vim.api.nvim_buf_set_keymap
+vim.opt_local.wrap = true -- wrap lines
 
 -- follow md links
+local nvim_buf_set_keymap = vim.api.nvim_buf_set_keymap
 nvim_buf_set_keymap(0, 'n', '<cr>', ':lua require("follow-md-links").follow_link()<cr>', {noremap = true, silent = true})
